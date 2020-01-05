cfcbb67cd87ab1d9e58f4f77b40d2af065ed4b1e
Anna Ksiazek <aksiazek@softak.eu>
2020-01-05 13:08:32 +0100

    add exceptions' tests (assertJ)

--- src/test/java/pl/sdacademy/tdd/examples/PowerCalculatorTest.java
+++ src/test/java/pl/sdacademy/tdd/examples/PowerCalculatorTest.java
@@ -1,6 +1,8 @@
 package pl.sdacademy.tdd.examples;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,5 +44,30 @@ class PowerCalculatorTest {
 		BigInteger result = powerCalc.calculate(base, power);
 
 		assertThat(result).isEqualTo(BigInteger.valueOf(expected));
+	}
+
+	@Test
+	void shouldThrowExceptionIfBaseNull() {
+		// blok given
+		BigInteger power = BigInteger.valueOf(2);
+
+		// w tym przypadku blok when i then są połączone. Wynika to faktu,
+		// że działanie normalnie realizowane w bloku when, w przypadku wyjątków
+		// jest od razu przedmiotem testu
+		// assetJ pozwala nie tylko kontrolować fakt wystąpienia wyjątku, jego typu ale również sprawdzać
+		// inne cechy (w tym przypadku komunikat) w jednym złożonym poleceniu
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> powerCalc.calculate(null, power))
+				.withMessage("Base cannot be null");
+
+	}
+
+	@Test
+	void shouldThrowExceptionIfPowerNull() {
+		BigInteger base = BigInteger.valueOf(2);
+
+		// skórcona forma kontroli określonego typu wyjątku (patrz przykład wyżej)
+		assertThatIllegalArgumentException().isThrownBy(() -> powerCalc.calculate(base, null))
+				.withMessage("Power cannot be null");
+
 	}
 }
